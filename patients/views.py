25dddc4ab5118b178d826721cb07fe34dd19d797
smenendez <smenendez@localhost.localdomain>
2026-06-23 13:58:40 +0200

    Class08_01

--- patients/views.py
+++ patients/views.py
@@ -1,13 +1,46 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .serializers import PatientSerializer
 from .models import Patient
 
-@api_view(["GET"])
+# GET /api/patients/ - List all patients
+# POST /api/patients/ - Create a new patient
+# GET /api/patients/<pk>/ - Retrieve a specific patient
+# PUT /api/patients/<pk>/ - Update a specific patient
+# DELETE /api/patients/<pk>/ - Delete a specific patient
+
+@api_view(["GET", "POST"])
 def ListPatients(request):
-    patients = Patient.objects.all()
-    serializer = PatientSerializer(patients, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PatientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        #return Response(serializer.data, status=201)
+        return Response(status=status.HTTP_201_CREATED)
+        #return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT", "DELETE"])
+def DetailPatient(request, pk):
+    try:
+        patient = Patient.objects.get(id=pk)
+    except Patient.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
+    if request.method == "PUT":
+        serializer = PatientSerializer(patient, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if request.method == "DELETE":
+        patient.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 '''
 from rest_framework.generics import (
     ListAPIView,
