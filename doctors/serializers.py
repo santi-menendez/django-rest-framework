082a13231b9ead5a792ff2172e79a7d62228f8fe
santi-menendez <santi.menendez@upc.edu>
2026-06-26 17:47:02 +0200

    Class17_v02

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -1,12 +1,30 @@
 from rest_framework import serializers
 
-from .models import Doctor, Department, DoctorAvailability, MedicalNote
+from bookings.serializers import AppointmentSerializer, MedicalNoteSerializer
+
+from .models import Department, Doctor, DoctorAvailability, MedicalNote
 
 
 class DoctorSerializer(serializers.ModelSerializer):
+    appointments = AppointmentSerializer(many=True, read_only=True)
+    notes = MedicalNoteSerializer(many=True, read_only=True)
+
     class Meta:
         model = Doctor
-        fields = '__all__'
+        fields = "__all__"
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "qualification",
+            "contact_number",
+            "email",
+            "address",
+            "biography",
+            "is_on_vacation",
+            "notes",
+            "appointments",
+        ]
 
     def validate_email(self, value):
         if "@example.com" in value:
@@ -14,7 +32,7 @@ class DoctorSerializer(serializers.ModelSerializer):
         raise serializers.ValidationError("El correo debe incluir @example.com")
 
     def validate(self, attrs):
-        if len(attrs['contact_number']) < 10 and attrs['is_on_vacation']:
+        if len(attrs["contact_number"]) < 10 and attrs["is_on_vacation"]:
             raise serializers.ValidationError(
                 "Por favor, ingrese un número válido antes de irte a vacaciones"
             )
@@ -24,16 +42,16 @@ class DoctorSerializer(serializers.ModelSerializer):
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
-        fields = '__all__'
+        fields = "__all__"
 
 
 class DoctorAvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
         model = DoctorAvailability
-        fields = '__all__'
+        fields = "__all__"
 
 
 class MedicalNoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalNote
-        fields = '__all__'
+        fields = "__all__"
