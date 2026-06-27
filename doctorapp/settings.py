8e0355fc190f6bc377ce842acfdd490c747e406c
santi-menendez <santi.menendez@upc.edu>
2026-06-27 19:38:02 +0200

    Class21_v01

--- doctorapp/settings.py
+++ doctorapp/settings.py
@@ -133,4 +133,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "5/minute",
+        "user": "100/minute",
+    },
 }
